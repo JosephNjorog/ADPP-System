4995091ad2d5df4b1ef3c58cd1e68a79e595f66e
JosephNjorog <njorojoe11173@gmail.com>
2024-08-27 21:10:02 +0300

    I am very confused right now

--- backend/src/config/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    port: process.env.PORT || 5000,
-    dbUri: process.env.MONGO_URI,
-    jwtSecret: process.env.JWT_SECRET,
-    apiBaseUrl: process.env.API_BASE_URL || '/api',
-};
--- backend/src/helpers/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    asyncHandler: fn => (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    },
-    generateRandomString: length => {
-        return Math.random().toString(36).substr(2, length);
-    },
-};
